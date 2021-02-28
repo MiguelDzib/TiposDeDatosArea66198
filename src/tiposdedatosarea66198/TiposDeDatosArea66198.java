9baa909e785d6813cd05f7fd6d56775efb37c247
galbe <galbe@192.168.1.68>
2021-02-28 09:44:31 -0600

    Tipos de datos area 66198

---
+++ src/tiposdedatosarea66198/TiposDeDatosArea66198.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tiposdedatosarea66198;
+
+public class TiposDeDatosArea66198 {
+
+    public static void main(String[] args) {
+        
+    int altura=5 ;
+    int base=8 ;
+    int area= altura * base ;
+    int perimetro= 2*altura+2*base ;
+    System.out.print("AREA= "+ area +" m^2 ") ;
+    System.out.print(" PERIMETRO= " + perimetro +" m");
+        
+        
+    }
+    
+}
